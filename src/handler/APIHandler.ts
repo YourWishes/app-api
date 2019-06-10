5eb1bc6f44b9963c58a2ba08ab33f7658dedf209
Dominic Masters <dominic@domsplace.com>
2019-06-10 14:28:13 +1000

    Updated deps, impl and testing new absolute URL system.

--- src/handler/APIHandler.ts
+++ src/handler/APIHandler.ts
@@ -21,8 +21,8 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import { APIResponse } from './../response/';
-import { APIRequest } from './../request/';
+import { APIResponse } from '~response';
+import { APIRequest } from '~request';
 
 export abstract class APIHandler {
   paths:string[];
